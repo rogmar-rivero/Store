1c545a09f01ede8c812bf71d8836e765ce9e9ee6
Rogmar <rogmarramsesriveromoyetones@gmail.com>
2022-02-05 16:52:04 -0400

    cambios genericos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicialComponent } from './pages/inicial/inicial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicial', component: InicialComponent },
+  { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule)},
+  { path: '**',redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
