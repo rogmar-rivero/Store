95e79ca07a6b9c5718b4d0b614705ba7a1c73902
Rogmar <rogmarramsesriveromoyetones@gmail.com>
2022-02-05 16:51:42 -0400

    componente inicial

---
+++ src/app/pages/inicial/inicial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-inicial',
+  templateUrl: './inicial.component.html',
+  styleUrls: ['./inicial.component.scss']
+})
+
+export class InicialComponent {
+
+}
