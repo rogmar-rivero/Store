b2ba53f05e3d6aab84c762408c7d9860b0b02f9b
Rogmar <rogmarramsesriveromoyetones@gmail.com>
2022-02-13 20:18:20 -0400

    components Material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   exports: [
     MatToolbarModule,
-    
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule
   ],
 })
 
