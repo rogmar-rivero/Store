0593c2653ff7398ed374d5938ed9a3f118065e95
Rogmar <rogmarramsesriveromoyetones@gmail.com>
2022-02-13 20:25:10 -0400

    Header Component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ShoppingCartServices } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  quantity$= this.shoppinCartSvc.quantityAction$;
+  cart$= this.shoppinCartSvc.cartAction$;
+  total$= this.shoppinCartSvc.totalAction$;
 
+  constructor(private route: Router, private shoppinCartSvc: ShoppingCartServices){}
+
+  goToCheckout(){
+    this.route.navigate(['/checkout'])
+  }
 }
